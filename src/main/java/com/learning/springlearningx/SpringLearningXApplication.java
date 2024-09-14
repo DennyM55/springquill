dd47c3cd0a19bd9f8fe7127610cc5ccc3edc9188
Denny M <165308292+dennymathew25@users.noreply.github.com>
2024-09-14 20:20:51 +0530

    git init

---
+++ src/main/java/com/learning/springlearningx/SpringLearningXApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springlearningx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearningXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/springlearningx/SpringLearningXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springlearningx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
